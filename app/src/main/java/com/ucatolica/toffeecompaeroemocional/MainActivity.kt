0e022951b0c45a3863bdfccaf54c43055325289f
ccgomez97 <ccgomez97@ucatolica.edu.co>
2022-10-13 18:32:48 -0500

    Inicio se sesión estilizado

--- app/src/main/java/com/ucatolica/toffeecompaeroemocional/MainActivity.kt
+++ app/src/main/java/com/ucatolica/toffeecompaeroemocional/MainActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.text.util.Linkify
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -18,16 +20,22 @@ class MainActivity : AppCompatActivity() {
 
         val textEmail:EditText = findViewById(R.id.editTextEmail)
         val textPass:EditText = findViewById(R.id.editTextPassword)
-
         val logInButton = findViewById<Button>(R.id.logInButton)
+        val textNoCuenta = findViewById<TextView>(R.id.textViewNoCuenta)
+        val textOlvideContra = findViewById<TextView>(R.id.textViewOlvideContraseña)
+
         logInButton.setOnClickListener {
             if (textEmail.text.isNotEmpty() && textPass.text.isNotEmpty()){
                 logIn(textEmail.text.toString(), textPass.text.toString())
             }else{
                 Toast.makeText(this, "Campos vacíos", Toast.LENGTH_LONG).show()
             }
-
         }
+
+        textNoCuenta.setMovementMethod(LinkMovementMethod.getInstance())
+        textOlvideContra.setMovementMethod(LinkMovementMethod.getInstance())
+
+        //Linkify.addLinks(textNoCuenta, "www.google.com")
 
     }
 
