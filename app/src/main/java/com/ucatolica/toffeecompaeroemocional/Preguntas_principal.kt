cb53b0d992d3cf5f797bd42041eb0315a77ad886
ccgomez97 <ccgomez97@ucatolica.edu.co>
2022-10-28 11:18:48 -0500

    Indicador de selección

--- app/src/main/java/com/ucatolica/toffeecompaeroemocional/Preguntas_principal.kt
+++ app/src/main/java/com/ucatolica/toffeecompaeroemocional/Preguntas_principal.kt
@@ -3,16 +3,20 @@ package com.ucatolica.toffeecompaeroemocional
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.MutableFloat
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.facebook.shimmer.ShimmerFrameLayout
+import com.google.firebase.database.MutableData
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
 
 class Preguntas_principal : AppCompatActivity() {
 
     val db = FirebaseFirestore.getInstance()
+    val listaRespuestas = mutableListOf<Respuesta>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,10 +44,21 @@ class Preguntas_principal : AppCompatActivity() {
                 container.hideShimmer()
                 container.visibility = View.GONE
                 rvPreguntas.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-                rvPreguntas.adapter = PreguntaAdapter(listaPreguntas)
+                rvPreguntas.adapter = PreguntaAdapter(listaPreguntas, { respuestasPreguntas(it)})
             }
             .addOnFailureListener { exception ->
                 Log.d("Preguntas", "Error getting documents: $exception", exception)
             }
+    }
+
+    fun respuestasPreguntas( respuesta: List<Float>) {
+        val objeto = Respuesta()
+        objeto.respuesta = respuesta[1]
+        objeto.position = respuesta[0].toInt()
+       listaRespuestas.add(objeto)
+       //listaRespuestas.getOrElse(respuesta[0].toInt())
+
+        Toast.makeText(this, "$listaRespuestas", Toast.LENGTH_SHORT).show()
+        Log.d("Respuestas", "Lista de respuestas $listaRespuestas")
     }
 }
---
+++ app/src/main/java/com/ucatolica/toffeecompaeroemocional/Respuesta.kt
@@ -0,0 +1,4 @@
+package com.ucatolica.toffeecompaeroemocional
+
+
+data class Respuesta (var position: Int= 0, var respuesta: Float= 0f)
